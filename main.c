77670c72e8ab25dbd630c08d383d54a837ed5792
Hammilk <khai1995pham@gmail.com>
2024-04-29 16:16:54 -0500

    f3 added

--- main.c
+++ main.c
@@ -5,11 +5,13 @@
 
 void f1();
 void f2();
+void f3();
 
 int main(int argc, char *argv[])
 {
-    f1();
-    f2();
+    //f1();
+    //f2();
+    f3();
 
     return EXIT_SUCCESS;
 }
@@ -50,3 +52,26 @@ void f2(){
     printf("Stack Size   #%d   is %li\n", n, n * arrSize);
     f2();
 }
+
+void f3(){
+    //Increments number of activation records
+    static int n = 0;
+
+    //Array of whatever
+    char* arr;
+    arr = (char *) malloc(SIZE * sizeof(char));
+    //Stores the starting address of the array
+    static long int addr;
+    long int c = sizeof(arr);  
+   
+    n++;
+    addr = (long int)&arr;
+    printf("Call #%d   at %p\n", n, arr);
+    printf("AR Size  #%d  is %li\n", n, c);
+    if(n < 10){
+        f3();
+    }
+    free(arr);
+}
+
+
