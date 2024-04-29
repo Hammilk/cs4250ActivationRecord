b36a53e88941065453e942246e826c77f67e61a2
Hammilk <khai1995pham@gmail.com>
2024-04-29 16:05:45 -0500

    f2 added

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #define SIZE 1000
 
 void f1();
+void f2();
 
 int main(int argc, char *argv[])
 {
-    f1();    
+    f1();
+    f2();
 
     return EXIT_SUCCESS;
 }
@@ -29,4 +31,22 @@ void f1(){
     if(n < 10){
         f1();
     }
+}
+
+void f2(){
+    //Increments number of activation records
+    static int n = 0;
+    //Array of whatever
+    char arr[SIZE];
+    //Stores the starting address of the array
+    static long int addr;
+    
+    long int arrSize = addr - (long int)&arr;
+   
+    n++;
+    addr = (long int)&arr;
+    printf("Call #%d   at %p\n", n, arr);
+    printf("AR Size  #%d  is %li\n", n, arrSize);
+    printf("Stack Size   #%d   is %li\n", n, n * arrSize);
+    f2();
 }
