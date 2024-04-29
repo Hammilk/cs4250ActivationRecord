e79ba9d27c52a025df9a7b5d4286b1821a061838
Hammilk <khai1995pham@gmail.com>
2024-04-29 15:53:11 -0500

    init commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 1000
+
+void f1();
+
+int main(int argc, char *argv[])
+{
+    f1();    
+
+    return EXIT_SUCCESS;
+}
+
+void f1(){
+    //Increments number of activation records
+    static int n = 0;
+    //Array of whatever
+    char arr[SIZE];
+    //Stores the starting address of the array
+    static long int addr;
+    
+    long int arrSize = addr - (long int)&arr;
+   
+    n++;
+    addr = (long int)&arr;
+    printf("Call #%d   at %p\n", n, arr);
+    printf("AR Size  #%d  is %li\n", n, arrSize);
+    if(n < 10){
+        f1();
+    }
+}
